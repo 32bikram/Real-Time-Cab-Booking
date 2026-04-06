d90088749364455c47d8fab79b371ec277d0978b
32bikram <bikramsarkareveland@gmail.com>
2026-04-06 18:13:04 +0000

    updated server ip

--- app.py
+++ app.py
@@ -124,5 +124,5 @@ def make_payment(data):
 # RUN SERVER
 # -------------------------------
 if __name__ == "__main__":
-    socketio.run(app, debug=True)
-    #socketio.run(app, host="0.0.0.0", port=5000, debug=True)
+    # socketio.run(app, debug=True)
+    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
